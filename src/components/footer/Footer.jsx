79ed778c775f121ac9a6b34e14bb1fec1ebbc6c8
Anirban Gorain <gorainanirban@gmail.com>
2024-01-16 18:14:43 +0530

    Footer is done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React from "react";
+import { FaFacebookF, FaInstagram, FaTwitter,FaLinkedin } from "react-icons/fa";
+import { Wrapper } from "../wrapper/Wrapper";
+import "./style.scss";
 
-export const Footer = () =>
+export const Footer = () => 
 {
-  return (
-    <div>
-      <h1>Footer</h1>
-    </div>
-  )
-}
+    return (
+        <footer className="footer">
+            <Wrapper>
+                <ul className="menu-items">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+                <div className="info-text">
+                All content included on this site in or made available through any TMDb Service, such as text, graphics, logos, button icons, images, audio clips, video clips, digital downloads, data compilations, and software, is the property of IMDb or its content suppliers and protected by United States and international copyright laws.
+                </div>
+                <div className="social-icons">
+                    <span className="icon">
+                        <FaFacebookF />
+                    </span>
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+                    <span className="icon">
+                        <FaTwitter />
+                    </span>
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                </div>
+            </Wrapper>
+        </footer>
+    );
+};
