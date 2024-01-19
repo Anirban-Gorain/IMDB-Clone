10fca92bca96bf61004779340f1d3e082e90e582
Anirban Gorain <gorainanirban@gmail.com>
2024-01-19 19:32:53 +0530

    Play button is done

---
+++ src/components/playButton/PlayButton.jsx
@@ -0,0 +1,12 @@
+import "./style.scss";
+
+import React from 'react'
+
+export const PlayButton = () => 
+{
+  return (
+    <div class="video-play-button">
+        <span></span>
+    </div>
+  )
+}
