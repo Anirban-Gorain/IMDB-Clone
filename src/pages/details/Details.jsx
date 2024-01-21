aaade8d499edec7df3e59dae5319a916cf2e98f9
Anirban Gorain <gorainanirban@gmail.com>
2024-01-21 13:10:51 +0530

    Shifted the VideoPlayer component from DetailsBanner to Details

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { Wrapper } from "../../components/wrapper/Wrapper";
@@ -6,6 +6,7 @@ import { DetailsBanner } from "./detailsBanner/DetailsBanner";
 import { Cast } from "./cast/Cast";
 import { useParams } from "react-router-dom"; 
 import { fetchAPI } from "../../customHooks/customHooks";
+import { VideoPlayer } from "../../components/videoPlayer/VideoPlayer.jsx";
 
 
 import "./style.scss";
@@ -15,22 +16,29 @@ export const Details = () =>
 	const {mediaType, id} = useParams();
 	const {result, isLoading} = fetchAPI(`/${mediaType}/${id}`);
 	const {result: workers, isLoading: isLoadingCrew} = fetchAPI(`/${mediaType}/${id}/credits`);
-	const {result: videoResult, isLoading: isLoadingResult} = fetchAPI(`/${mediaType}/${id}/videos`);
+	const {result: videoResult, isLoading: isLoadingVideoResult} = fetchAPI(`/${mediaType}/${id}/videos`);
+	const [show, setShow] = useState(false);
+	const [videoId, setVideoId] = useState(null);
 	
 	// Filtering all the YT videos
 
 	const ytVideos = videoResult?.results?.filter((val) => val.site === "YouTube");
 
-	console.log(workers);
-
 	return (
 		<>
-			<DetailsBanner 
-				isLoading={(isLoading || isLoadingCrew || isLoadingResult)} 
+			{
+				ytVideos?.length>0 && <VideoPlayer setShow={setShow} show={show} videoId={videoId}/>
+			}
+			{
+				ytVideos?.length>0 && <DetailsBanner 
+				isLoading={(isLoading || isLoadingCrew || isLoadingVideoResult)} 
 				result={result}  
 				crewResult={workers}
-				videoResult={ytVideos}
-			/>
+				videoId={ytVideos[0]?.key}
+				setVideoId={setVideoId}
+				setShow={setShow}
+				/>
+			}
 			<Cast isLoading={(isLoadingCrew)} casts={workers?.cast}/>
 		</>
 	)
