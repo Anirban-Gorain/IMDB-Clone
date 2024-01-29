30f87b034e6ea6cd14db5ca33398747bf0fcb2cd
Anirban Gorain <gorainanirban@gmail.com>
2024-01-29 08:11:31 +0530

    Fix the URL updation issue in the Explore

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Wrapper } from "../../components/wrapper/Wrapper";
+import { fetchDataFromApi } from "../../utils/api";
+import { MovieCard } from "../../components/movieCard/MovieCard";
+import { LoadingCircle } from "../../components/loadingCircle/LoadingCircle.jsx";
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { FaRobot } from "react-icons/fa6";
+import { useNavigate } from "react-router";
 import Select from 'react-select';
 import dayjs from "dayjs";
+
 
 import "./style.scss";
 
@@ -12,12 +19,16 @@ export const Explore = () =>
 {
 	const {mediaType} = useParams();
 	const [exploreResults, setExploreResults] = useState([]);
+	const [pageNumber, setPageNumber] = useState(1);
+	const [totalRes, setTotalRes] = useState(1);
+	const [url, setUrl] = useState();
+	const navigate = useNavigate();
 	const {genres} = useSelector((state) => state.home);
 	// Options
 
 	const lastYear = dayjs().format("YYYY")
 	const yearOptions = [...Array(lastYear - 1900 + 1)].map((elem, index) => {return {value: 1900 + index, label: 1900 + index}});
-	const [year, setYear] = useState(lastYear);
+	const [year, setYear] = useState("all");
 	
 	const [popularity, setPopularity] = useState("popularity.desc");
 	const popularOptions = [
@@ -28,6 +39,49 @@ export const Explore = () =>
 	const [includeGenres, setIncludeGenres] = useState("all");
 	const [excludeGenres, setExcludeGenres] = useState("none");
 	const genresOptions = Object.values(genres).map((elem) => {return {value: elem.toLowerCase(), label: elem}});
+
+	
+	console.log(exploreResults);
+	
+	useEffect(() => {
+		setExploreResults([]);
+		const constructURL = `/discover/${mediaType === "movies" ? "movie" : "tv"}?${year === "all" ? "" : (mediaType === "movies" ? "primary_release_year=" : "first_air_date_year=") + year}&sort_by=${popularity}${includeGenres === "all" ? "" : "&with_genres=" + includeGenres}${excludeGenres === "none" ? "" : "&without_genres=" + excludeGenres}&page=${1}`;
+		setUrl(constructURL);
+
+		(async () => {
+			try {
+				const response = await fetchDataFromApi(constructURL);
+				setExploreResults(response.results);
+				setTotalRes(response.total_results);
+				setPageNumber(2);
+	
+			} catch (error) {
+				console.log(error);
+			}
+		})();
+	}, [year, popularity, includeGenres, excludeGenres, mediaType]);
+
+	async function nextMovieResults()
+	{
+		try 
+		{
+			const response = await fetchDataFromApi(url);
+			console.log(response);
+			setPageNumber((prev) => prev+1);
+			setExploreResults((prev) => 
+			{
+				const newData = [...prev, ...response.results];
+				return newData;
+			});
+			
+		} 
+		catch (error) 
+		{
+			console.log(error);	
+		}
+	}
+
+	console.log(exploreResults);
 
 	return (
 		<div className="explore-container">
@@ -42,8 +96,8 @@ export const Explore = () =>
 								<label htmlFor="year">Year</label>
 								<Select 
 									id="year"
-									options={yearOptions} 
-									defaultValue={[{value: lastYear, label: lastYear}]}
+									options={[{value: "all", label: "All"}, ...yearOptions]} 
+									defaultValue={[{value: "all", label: "All"}]}
 									onChange={(e) => setYear(e.value)}
 									className={"filter"}
 								/>
@@ -85,7 +139,33 @@ export const Explore = () =>
 				</div>
 
 				<div className="explore-items">
-					<h1>Items</h1>
+					{
+						exploreResults?.length != 0  && <InfiniteScroll
+							dataLength={exploreResults?.length || []}
+							next={nextMovieResults}
+							hasMore={exploreResults?.length!=totalRes}
+							loader={<LoadingCircle />}
+						>
+							{
+								exploreResults?.map((result, ind) => 
+								{
+									return(
+										<MovieCard 
+											key={ind} 
+											result={result} 
+											clickHandler={()=>navigate(`/${mediaType == "movies" ? "movie" : "tv"}/${result?.id}`)}
+										/>
+									)
+								})
+							}
+						</InfiniteScroll>
+					}
+
+					{
+						exploreResults?.length == 0 && <LoadingCircle />
+					}
+
+
 				</div>
 			</Wrapper>
 		</div>
