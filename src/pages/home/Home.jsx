a3139bfa91c69d73be9bc465e0877d995f7743b0
Anirban Gorain <gorainanirban@gmail.com>
2024-01-30 08:04:20 +0530

    Fixing build issues

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { HeroBanner } from "./heroBanner/HeroBanner";
 import { Trending } from "./trending/Trending";
+import { HeroBanner } from "./heroBanner/HeroBanner";
 import { Popular } from "./popular/Popular";
 import { TopRated } from "./topRated/TopRated";
 
